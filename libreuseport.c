5ab561ab1ba84c0a22d68fb26b21e1613674f6d5
Michael Santos <michael.santos@gmail.com>
2025-03-07 07:29:31 -0500

    doc: libreuseport: update copyright date

--- libreuseport.c
+++ libreuseport.c
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2018-2023 Michael Santos
+ * Copyright (c) 2018-2025 Michael Santos
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
